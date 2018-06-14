c797d13531fde7f9c571610a6923f45db66c4e08
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-13 22:40:49 -0400

    updated login

---
+++ src/containers/Login.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import * as actions from "../actions/Action";
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        let usernameFld
+        let passwordFld
+
+        return (
+            <div>
+                <h1>Login Page</h1>
+                <input placeholder='Username' value={this.props.username} onChange={() => {
+                    this.props.changeUsername(usernameFld.value)
+                }}
+                       ref={node => usernameFld = node} />
+
+                <input placeholder='Password' type='password' value={this.props.password} onChange={() => {
+                    this.props.changePassword(passwordFld.value)
+                }}
+                       ref={node => passwordFld = node} />
+
+                <button onClick={()=>{this.props.doLogin(this.props.username, this.props.password)}}>
+                    Login
+                </button>
+            </div>
+        )
+    }
+}
+
+const stateToPropertyMapper = (state) => ({
+    username: state.LoginDetails.username,
+    password: state.LoginDetails.password
+});
+
+export const dispatcherToPropsMapper = (dispatch) => ({
+    changeUsername: (username) => actions.changeUsername(dispatch,username),
+    changePassword: (password) => actions.changePassword(dispatch,password),
+    doLogin: (username,password) => actions.doLogin(dispatch,username,password)
+});
+
+
+const loginContainer = connect(stateToPropertyMapper, dispatcherToPropsMapper)(Login)
+
+export default loginContainer
