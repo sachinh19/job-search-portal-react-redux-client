6cba1694119e66c6bb2d622280246782717de41a
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-14 10:37:14 -0400

    updated root reduder

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,9 +1,13 @@
 import {combineReducers} from 'redux';
-
+import LoginReducer from './LoginReducer'
+import UserReducer from './UserReducer'
+import AlertReducer from './AlertReducer'
 
 
 const RootReducer = combineReducers({
-
+    LoginReducer,
+    UserReducer,
+    AlertReducer
 })
 
 export default RootReducer;
