86020038e6736189102d281947a32062483d609e
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-14 15:14:14 -0400

    added NavBar component

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import HomeContainer from './containers/Home'
 import LoginContainer from './containers/Login'
-import {Route,Swithc} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import {Route,Switch} from 'react-router-dom'
+
 
 export default class Routes extends Component {
     render(){
         return(
-            <div>
+            <div class="container">
                 <div className={"navbar"}>
-                    Header
+                    <NavBar/>
                 </div>
                 <Route exact path={"/"} component={HomeContainer}/>
                 <Route exact path={"/login"} component={LoginContainer}/>
