511aca136f02ea99be3bb76ec3d251bbd673c0fa
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-13 22:34:35 -0400

    Added home container

---
+++ src/containers/Home.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom';
+import * as actions from "../actions/Action";
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    renderLogin() {
+        if(this.props.userId !== null){
+            return <button onClick={this.props.logOut}>LogOut</button>
+        } else {
+            return <Link to={`/login`}>Login</Link>
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+                {this.renderLogin()}
+            </div>
+        )
+    }
+}
+
+const stateToPropertyMapper = (state) => ({
+
+    userId:localStorage.getItem('id')
+})
+
+export const dispatcherToPropsMapper = (dispatch) => ({
+    logOut: () => actions.logOut(dispatch)
+})
+
+
+const homeContainer = connect(stateToPropertyMapper, dispatcherToPropsMapper)(Home)
+
+export default homeContainer
