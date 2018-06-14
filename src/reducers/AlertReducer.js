f96fb8b6666ad9c503ea06355fbef2bc597ce455
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-14 10:34:03 -0400

    added reducer files

---
+++ src/reducers/AlertReducer.js
@@ -0,0 +1,12 @@
+import * as constants from '../constants/constant'
+
+const Alert = (state = null, action) => {
+
+    switch (action.type) {
+
+        default :
+            return state
+    }
+};
+
+export default Alert;
