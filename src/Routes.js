d3a6e48d685370a737ce3722cbac739bcccd0edb
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-13 23:14:09 -0400

    added Routes

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'React';
+import HomeContainer from './containers/Home'
+import LoginContainer from './containers/Login'
+import {Route,Swithc} from 'react-router-dom'
+
+export default class Routes extends Component {
+    render(){
+        return(
+            <div>
+                <div className={"navbar"}>
+                    Header
+                </div>
+                <Route exact path={"/"} component={HomeContainer}/>
+                <Route exact path={"/login"} component={LoginContainer}/>
+                <div>
+                    Footer
+                </div>
+            </div>
+        )
+    }
+}
