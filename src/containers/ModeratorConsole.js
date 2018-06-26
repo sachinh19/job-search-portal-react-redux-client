a0d0477e493d197734b1a6559d3d2af904492f9b
sachinh19 <sachinhaldavanekar@gmail.com>
2018-06-26 13:55:44 -0400

    Merge branch 'master' of https://github.com/sachinh19/webdev-cs5610-project-team-2070
    # Conflicts:
    #	src/components/Profile.js
    
    Added Most Recently Joined Job Seekers and Jobs on the Home Page
    
    Added Styling and changed displayed values for Job seekers
    
    Added access forbidden to Moderator

--- src/containers/ModeratorConsole.js
+++ src/containers/ModeratorConsole.js
@@ -4,6 +4,7 @@ import {Route,Link} from 'react-router-dom'
 import '../styles/AdminConsole.css'
 import AdminConsoleJobComponent from '../components/AdminConsoleJobComponent'
 import AdminConsoleCompaniesComponent from '../components/AdminConsoleCompaniesComponent'
+import AdminConsoleUsersContainer from "../components/AdminConsoleUserComponent";
 
 
 class ModeratorConsole extends Component {
@@ -12,28 +13,34 @@ class ModeratorConsole extends Component {
     }
 
     render() {
-        return (
-            <div className="row">
-                <div className="col-md-3">
-                    <div className="container wbdv-options-container">
-                        <ul className="list-group">
-                            <li className="list-group-item wbdv-options">
-                                <Link to={`/moderatorConsole/jobs`} className="wbdv-options-item">View All Jobs</Link>
-                            </li>
-                            <li className="list-group-item wbdv-options">
-                                <Link to={`/moderatorConsole/companies`} className="wbdv-options-item">View All Companies</Link>
-                            </li>
-                        </ul>
+        if (localStorage.getItem("userRole") === "Moderator") {
+            return (
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="container wbdv-options-container">
+                            <ul className="list-group">
+                                <li className="list-group-item wbdv-options">
+                                    <Link to={`/moderatorConsole/jobs`} className="wbdv-options-item">View All
+                                        Jobs</Link>
+                                </li>
+                                <li className="list-group-item wbdv-options">
+                                    <Link to={`/moderatorConsole/companies`} className="wbdv-options-item">View All
+                                        Companies</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-9">
+                        <div className="wbdv-option-list-container">
+                            <Route path={"/moderatorConsole/jobs"} component={AdminConsoleJobComponent}/>
+                            <Route path={"/moderatorConsole/companies"} component={AdminConsoleCompaniesComponent}/>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-9">
-                    <div className="wbdv-option-list-container">
-                        <Route path={"/moderatorConsole/jobs"} component={AdminConsoleJobComponent}/>
-                        <Route path={"/moderatorConsole/companies"} component={AdminConsoleCompaniesComponent}/>
-                    </div>
-                </div>
-            </div>
-        )
+            )
+        } else {
+            return (<div><h1 className={"wbdv-access-forbidden"}><i>Access Forbidden</i></h1></div>)
+        }
     }
 }
 
